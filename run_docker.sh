a82ce6236e6daff441d3801cf90482d5c70e7a70
marouenj <marouen.jilani@gmail.com>
2016-05-06 18:47:56 +0900

    run as net=host

--- run_docker.sh
+++ run_docker.sh
@@ -3,5 +3,6 @@
 docker run -d \
        --privileged \
        --name=inner-docker \
-       -p 2380:2375 \
-docker:dind 
+       --net=host \
+docker:dind \
+docker daemon --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2380 --storage-driver=vfs
